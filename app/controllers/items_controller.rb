b0c81c1976612e2f108e82b8a4252c6f7117b348
swiftyy188 <jeffho.ml@gmail.com>
2018-08-05 14:24:30 -0400

    item model api adapter created

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,19 @@
 class ItemsController < ApplicationController
+	def index
+		url = "https://hacker-news.firebaseio.com/v0/"
+		response = HTTParty.get(base_uri + 'topstories.json')
+		response = response.parsed_response
+		@itemsList = []
+
+		response[0...2].each { |item|
+			listItem = HTTParty.get(url + 'topstories.json')
+			listItem = listItem.parsed_response
+			listItem['shortUrl']=parseHostUrl(listItem['url'])
+			listeItem['timeDifference']=Time.at(listItem['time'])
+			@items.push(listItem)
+		}
+	end
+	def parseHostUrl(url)
+		return URI.parse(url).host;
+	end
 end
