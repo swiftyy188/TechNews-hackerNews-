cd522f4b37d2e43d6819b060a3121b16356d49b0
swiftyy188 <jeffho.ml@gmail.com>
2018-08-11 14:28:33 -0400

    fixed all errrors

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,14 @@
 class ItemsController < ApplicationController
 	def index
 		url = "https://hacker-news.firebaseio.com/v0/"
-		response = HTTParty.get(base_uri + 'topstories.json')
+		response = HTTParty.get(url + 'topstories.json')
 		response = response.parsed_response
-		@itemsList = []
+		@items = []
 
 		response[0...2].each { |item|
-			listItem = HTTParty.get(url + 'topstories.json')
-			listItem = listItem.parsed_response
-			listItem['shortUrl']=parseHostUrl(listItem['url'])
-			listeItem['timeDifference']=Time.at(listItem['time'])
-			@items.push(listItem)
+			toBeStaged = HTTParty.get(url + 'item/' + item.to_s + '.json')
+			toBeStaged = toBeStaged.parsed_response
+			@items.push(toBeStaged);
 		}
-	end
-	def parseHostUrl(url)
-		return URI.parse(url).host;
 	end
 end
