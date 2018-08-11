6d122f81d103f837f5f420d46ee8892f7cc95e0a
swiftyy188 <jeffho.ml@gmail.com>
2018-08-11 15:57:45 -0400

    Added appropriate styles to item list & nav items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,17 @@
 class ItemsController < ApplicationController
 	def index
 		url = "https://hacker-news.firebaseio.com/v0/"
+		website = 'https://news.ycombinator.com/'
 		response = HTTParty.get(url + 'topstories.json')
 		response = response.parsed_response
 		@items = []
 
-		response[0...2].each { |item|
+		response[0...10].each { |item|
 			listItem = HTTParty.get(url + 'item/' + item.to_s + '.json')
 			listItem = listItem.parsed_response
+			listItem['shortUrl'] = parseHostUrl(listItem['url'])
+			listItem['timeDifference'] = Time.at(listItem['time'])
+			listItem['commentUrl'] = website + 'item?id' +item.to_s
 			@items.push(listItem);
 		}
 	end
