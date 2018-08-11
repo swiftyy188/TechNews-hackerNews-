f13b99d494f9ff462139b8b89825abcdd22f4d55
swiftyy188 <jeffho.ml@gmail.com>
2018-08-11 16:11:58 -0400

    refactor controller & views, applied correct styling

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,21 @@
 class ItemsController < ApplicationController
-	def index
-		url = "https://hacker-news.firebaseio.com/v0/"
-		website = 'https://news.ycombinator.com/'
-		response = HTTParty.get(url + 'topstories.json')
-		response = response.parsed_response
-		@items = []
+  def index
+    url = 'https://hacker-news.firebaseio.com/v0/'
+    website = 'https://news.ycombinator.com/'
+    response = HTTParty.get(url + 'topstories.json')
+    response = response.parsed_response
+    @itemsList = []
+    response[0...10].each { | item | 
+      listItem = HTTParty.get(url + 'item/' + item.to_s + '.json')
+      listItem = listItem.parsed_response
+      listItem['shortUrl'] = parseHostUrl(listItem['url'])
+      listItem['timeDifference'] = Time.at(listItem['time'])
+      listItem['commentUrl'] = website + 'item?id=' + item.to_s
+      @itemsList.push(listItem);
+    }
+  end
 
-		response[0...10].each { |item|
-			listItem = HTTParty.get(url + 'item/' + item.to_s + '.json')
-			listItem = listItem.parsed_response
-			listItem['shortUrl'] = parseHostUrl(listItem['url'])
-			listItem['timeDifference'] = Time.at(listItem['time'])
-			listItem['commentUrl'] = website + 'item?id' +item.to_s
-			@items.push(listItem);
-		}
-	end
-	# https://hacker-news.firebaseio.com/v0/item/17685358.json
-	def parseHostUrl(url)
-		return URI.parse(url).host;
-	end
+  def parseHostUrl(url)
+    return URI.parse(url).host;
+  end
 end
